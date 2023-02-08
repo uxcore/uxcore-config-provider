61623a7921aa6f4f23e069015321568c712bb07e
fanyixuan.fyx <fanyixuan.fyx@alibaba-inc.com>
2023-02-08 18:38:51 +0800

    feat:demo

--- demo/ConfigProviderDemo.jsx
+++ demo/ConfigProviderDemo.jsx
@@ -8,6 +8,8 @@
 
 import React from 'react';
 import ConfigProvider from '../src';
+import Button from 'uxcore-button';
+import Dialog from 'uxcore-dialog';
 import '../style';
 
 class Demo extends React.Component {
@@ -15,13 +17,49 @@ class Demo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      initDialogVisible: false,
+      localeDialogVisible: false
     };
+  }
+
+  openDialog(paramName) {
+    this.setState({ [paramName]: true });
+  }
+
+  closeDialog(paramName) {
+    this.setState({ [paramName]: false });
   }
 
   render() {
     return (
       <div>
-        <ConfigProvider />
+        <Button onClick={this.openDialog.bind(this, 'initDialogVisible')}>原始Dialog语言包</Button>
+        <Dialog
+          title="默认语言包"
+          content="内容"
+          onOk={this.closeDialog.bind(this, 'initDialogVisible')}
+          onCancel={this.closeDialog.bind(this, 'initDialogVisible')}
+          visible={this.state.initDialogVisible}
+        >
+          <div>123</div>
+        </Dialog>
+        <br />
+        <br />
+        <Button onClick={this.openDialog.bind(this, 'localeDialogVisible')}>传入自定义语言包Dialog</Button>
+        <ConfigProvider localePack={{
+          Dialog: {
+            ok: '自定义'
+          }
+        }}>
+          <Dialog
+            visible={this.state.localeDialogVisible}
+            title="自定义语言包"
+            onOk={this.closeDialog.bind(this, 'localeDialogVisible')}
+            onCancel={this.closeDialog.bind(this, 'localeDialogVisible')}
+          >
+            <div>123</div>
+          </Dialog>
+        </ConfigProvider>
       </div>
     );
   }
